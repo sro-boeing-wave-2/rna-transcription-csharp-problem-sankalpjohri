3d3672d03e3f7a89059604c95b497dbe5f6a55bd
Sankalp Johri <sankalp.johri@iiitb.org>
2018-07-17 16:33:01 +0530

    Initial Solution

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RNATranscriptionModule
 {
     public static class RnaTranscription
     {
+        private static Dictionary<char, char> rnaMapping = new Dictionary<char, char>(){
+            {'A', 'U'},
+            {'T', 'A'},
+            {'G', 'C'},
+            {'C', 'G'}
+        };
+
         public static string ToRna(string nucleotide)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            String rnaString = "";
+            if (!String.IsNullOrWhiteSpace(nucleotide))
+            {
+                foreach (char c in nucleotide)
+                {
+                    rnaString = rnaMapping.ContainsKey(c) ? String.Concat(rnaString, rnaMapping[c]) : rnaString;
+                }
+            }
+            return rnaString;
         }
     }
 }
